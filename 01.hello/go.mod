3c2e1b32223ac54e6addd17d7f952f167788a537
Vani Sharma <vani.7135@gmail.com>
2025-09-28 15:33:55 +0530

    01 hello  module

---
+++ 01.hello/go.mod
@@ -0,0 +1,3 @@
+module github.com/vanisharmaa/hello
+
+go 1.24.5
---
+++ 01.hello/main.go
@@ -0,0 +1,8 @@
+package main // Declares the package name
+
+import "fmt" // Imports the fmt package for formatted I/O
+
+// main is the entry point of the application
+func main() {
+	fmt.Println("Hello, world!") // Prints "Hello, world!" to the console
+}
