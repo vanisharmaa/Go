7b9c8439f4975aaf85c742616c9cdd7c86266c7b
Vani Sharma <vani.7135@gmail.com>
2025-10-08 07:33:13 +0530

    chore: implement simple node-express server

--- webserver/index.js
+++ webserver/index.js
@@ -11,21 +11,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.status(200).send("Welcome to LearnCodeonline server");
+  res.status(200).send("Welcome to Vani's Node in Go server");
 });
 
 app.get("/get", (req, res) => {
-  res.status(200).json({ message: "Hello from learnCodeonline.in" });
+  res.status(200).json({ message: "Hello from Vani's Node in Go server" });
 });
 
 app.post("/post", (req, res) => {
+  console.log("Hey, you hit /post route with ", req.body);
   let myJson = req.body; // your JSON
 
   res.status(200).send(myJson);
 });
 
 app.post("/postform", (req, res) => {
-  res.status(200).send(JSON.stringify(req.body));
+  res.status(200).send(JSON.stringify(req.body)); // form encode in request body instead of json
 });
 
 app.listen(port, () => {
