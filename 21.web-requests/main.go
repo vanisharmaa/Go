fd74341dcb4cedb0217ce365e295164e430db9f3
Vani Sharma <vani.7135@gmail.com>
2025-10-09 09:39:36 +0530

    21 web requests - GET/ POST/ (json and form)

--- 21.web-requests/main.go
+++ 21.web-requests/main.go
@@ -5,23 +5,30 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
-const url string = "http://localhost:8000/"
+const baseUrl string = "http://localhost:8000/"
 
 func main() {
 	fmt.Println("Welcome to web requests in Golang")
 
-	// const getUrl string = url + "get/"
+	const getUrl string = baseUrl + "get/"
 
 	// GET/ requests
-	/* PerformGetRequests(getUrl)
-	PerformGetRequestsMethod2(getUrl) */
+	PerformGetRequests(getUrl)
+	PerformGetRequestsMethod2(getUrl)
 
-	const postUrl string = url + "post/"
+	const postJsonUrl string = baseUrl + "post/"
 
-	PerformPostJsonRequest(postUrl)
+	// POST/ json request
+	PerformPostJsonRequest(postJsonUrl)
+
+	const postFormUrl string = baseUrl + "postform/"
+
+	// POST/ form request
+	PerformPostFormRequest(postFormUrl)
 
 }
 
@@ -36,12 +43,12 @@ func PerformGetRequests(myUrl string) {
 
 	defer response.Body.Close()
 
-	fmt.Println("Status code: ", response.StatusCode)
-	fmt.Println("Content Length: ", response.ContentLength)
+	fmt.Println("GET/ Status code: ", response.StatusCode)
+	fmt.Println("GET/ Content Length: ", response.ContentLength)
 
 	content, _ := io.ReadAll(response.Body)
-	fmt.Printf("type of response %T\n", content)
-	fmt.Println("response: ", string(content))
+	fmt.Printf("GET/ type of response %T\n", content)
+	fmt.Println("GET/ response: ", string(content))
 }
 
 func PerformGetRequestsMethod2(myUrl string) {
@@ -53,16 +60,16 @@ func PerformGetRequestsMethod2(myUrl string) {
 
 	defer response.Body.Close()
 
-	fmt.Println("Status code: ", response.StatusCode)
-	fmt.Println("Content Length: ", response.ContentLength)
+	fmt.Println("2. GET/ Status code: ", response.StatusCode)
+	fmt.Println("2. GET/ Content Length: ", response.ContentLength)
 
 	var responseString strings.Builder
 
 	content, _ := io.ReadAll(response.Body)
 	byteCount, _ := responseString.Write(content) // same as content length
 
-	fmt.Println("ByteCount is: ", byteCount)
-	fmt.Println("response: ", responseString.String()) // response
+	fmt.Println("2. GET/ ByteCount is: ", byteCount)
+	fmt.Println("2. GET/ response: ", responseString.String()) // response
 }
 
 func PerformPostJsonRequest(myUrl string) {
@@ -78,7 +85,7 @@ func PerformPostJsonRequest(myUrl string) {
 		}
 	`)
 
-	fmt.Printf("Type of request body = %T\n", requestBody)
+	fmt.Printf("Json POST/ Type of request body = %T\n", requestBody)
 	response, err := http.Post(myUrl, "Application/json", requestBody)
 
 	if err != nil {
@@ -93,5 +100,30 @@ func PerformPostJsonRequest(myUrl string) {
 		panic(err)
 	}
 
-	fmt.Println("content: ", string(content))
+	fmt.Println("Json POST/ content: ", string(content))
+}
+
+func PerformPostFormRequest(myUrl string) {
+	// form data
+	data := url.Values{}
+	data.Add("fname", "vani")
+	data.Add("lname", "sharma")
+	data.Add("age", "24")
+	data.Add("country", "india")
+
+	response, err := http.PostForm(myUrl, data)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	byteRes, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Form POST/ post form response: ", string(byteRes))
 }
